b225608b67b61d7f8b14987f1d7e8e6b274eb440
Cheryl Daniels <cheryld433@gmail.com>
2020-07-23 15:53:13 -0400

    added function to Container.js file

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import API from "../utils/API";
-import TableData from "../components/TableData";
-import SearchBox from "../components/SearchBox";
+import SearchBox from "./SearchBox"
+import TableData from "./TableData"
 import "./style.css";
 
 class Container extends Component {
+
     state = {
         search: "",
         employees: [],
@@ -12,23 +13,17 @@ class Container extends Component {
         order: ""
     };
 
-    // setState() enqueues(Adds to the queue) changes to the component state and tells React 
-    // that this component and its children need to be re-rendered with 
-    // the updated state.
     componentDidMount() {
-        API.getUsersInfo().then(res => this.setState({
+        API.getUsers().then(res => this.setState({
             employees: res.data.results,
             filteredEmployees: res.data.results
         })).catch(err => console.log(err))
     }
-
-
-
+    // names in ascending and descending order:
     sortByName = () => {
-        // filtered is looping:
         const filtereds = this.state.filteredEmployees;
-        if(this.state.order === "ascend") {
-            const sorteds = filtereds.sort((a, b) => (a.name.first > b.name.first) ? -1 : 1)
+        if (this.state.order === "ascend") {
+            const sorteds = filtereds.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
             console.log(sorteds)
 
             this.setState({
@@ -36,6 +31,7 @@ class Container extends Component {
                 order: "descend"
             })
         } else {
+
             const sorteds = filtereds.sort((a, b) => (a.name.first > b.name.first) ? -1 : 1)
             console.log(sorteds)
 
@@ -44,36 +40,37 @@ class Container extends Component {
                 order: "ascend"
             })
         }
-
-    };
-
+    }
+    
     handleInputChange = event => {
-        const employees = this.state.employees;
-        const userInput = event.target.value;
-        // ofIndex: return to first position;
-        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().indexOf(userInput.toLowerCase()) > -1 )
 
+        const employees = this.state.employees;
+        const UserInput = event.target.value;
+        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().indexOf(UserInput.toLowerCase()) > -1)
+        
         this.setState({
-            filteredEmployees, 
+            filteredEmployees,
         });
     };
-// API triggered at opening and refresh: 
-    employeeSearch = () => {
-        API.getUsersInfo() 
-        .then(res => this.setState({
-            // holds all data from the api call: 
-            filteredEmployees: res.data.result,
-            employee: res.data.result
-        }))
-    };
 
-    // when search button is clicked: 
+    //API call triggered at opening and refresh:
+    employeeSearch = () => {
+        API.getUsers()
+            .then(res => this.setState({
+
+                filteredEmployees: res.data.results,
+                employees: res.data.results
+            }))
+            .catch(err => console.log(err))
+    }
+
+    //when button search it's clicked:
     handleSearch = event => {
         event.preventDefault();
-        if(!this.state.search) {
-            alert("Please enter a name")
-        }    
-        const {employees, search} = this.state;
+        if (!this.state.search) {
+            alert("Enter a name")
+        }
+        const { employees, search } = this.state;
         const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().includes(search.toLowerCase()));
 
         this.setState({
@@ -81,20 +78,26 @@ class Container extends Component {
         });
     }
 
-    render(){
-        return(
-            <div>
-                <SearchBox
-                employee={this.state.employees}
-                handleSearch={this.handleSearch}
-                handleInputChange={this.handleInputChange} />
 
-                <TableData results= {this.state.filteredEmployees}
-                     sortByName={this.sortByName} />
-            </div>
+
+    render() {
+
+        return (
+            <div>
+
+                <SearchBox
+                    employee={this.state.employees}
+                    handleSearch={this.handleSearch}
+                    handleInputChange={this.handleInputChange} />
+                <TableData results={this.state.filteredEmployees}
+                    sortByName={this.sortByName}
+
+                />
+            </div >
+
+
         )
     }
 }
 
 export default Container;
-
