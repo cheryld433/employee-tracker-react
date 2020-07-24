1dc1612a72226db626f8086291a67f1a6d1d65e9
Cheryl Daniels <cheryld433@gmail.com>
2020-07-24 13:22:34 -0400

    Updates

---
+++ precache-manifest.625d4cd3b739372fbfcfe82093ba3420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c345783e81bf4d907f65b906f1b6006e",
+    "url": "/cheryld433/employee-tracker-react.git/index.html"
+  },
+  {
+    "revision": "1f59c1e11fccd30714e5",
+    "url": "/cheryld433/employee-tracker-react.git/static/css/main.f0dc7769.chunk.css"
+  },
+  {
+    "revision": "7c822a9ee49423872f3c",
+    "url": "/cheryld433/employee-tracker-react.git/static/js/2.c0131e85.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/cheryld433/employee-tracker-react.git/static/js/2.c0131e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f59c1e11fccd30714e5",
+    "url": "/cheryld433/employee-tracker-react.git/static/js/main.5c929b48.chunk.js"
+  },
+  {
+    "revision": "376fe992cf58702e2f1b",
+    "url": "/cheryld433/employee-tracker-react.git/static/js/runtime-main.c56d0663.js"
+  }
+]);
